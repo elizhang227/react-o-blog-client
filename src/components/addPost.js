63740f7bbf166477fb0b4390841dd246123936e7
elizhang227 <zhang.eli@hotmail.com>
2019-07-12 09:25:22 -0400

    wrapped input in form

--- src/components/addPost.js
+++ src/components/addPost.js
@@ -18,31 +18,35 @@ class AddPost extends Component {
         })
     }
 
-    handleSubmit = async (title) => {
-        console.log('this is working')
-        const url = 'http://localhost:3000/v1/post/add';
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(title)
-        })
-        const data = await response.json();
-        console.log('this is data', data)
-        return data;
-    }
+    // handleSubmit = async (title) => {
+    //     console.log('this is working')
+    //     const url = 'http://localhost:3000/v1/post/add';
+    //     const response = await fetch(url, {
+    //         method: 'POST',
+    //         headers: {
+    //             'Accept': 'application/json',
+    //             'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify(title)
+    //     })
+    //     const data = await response.json();
+    //     console.log('this is data', data)
+    //     return data;
+    // }
 
     render() {
         const { title, content } = this.state;
         console.log('this is title', title)
         return (
             <>
-                <input onChange={e => this.handleAddTitle(e)} placeholder='title'></input>
-                <button onClick={() => this.handleSubmit(title)}>submit</button>
-                <input onChange={e => this.handleAddContent(e)}placeholder='content'></input>
-                <button>submit</button>
+                <form action='http://localhost:3000/v1/post/add' method='POST'>
+                    <input onChange={e => this.handleAddTitle(e)} name='title' placeholder='title'></input>
+                    {/* <button onClick={() => this.handleSubmit(title)} type='submit'>submit</button> */}
+                    <input onChange={e => this.handleAddContent(e)} name='content' placeholder='content'></input>
+                    <button type='submit'>submit</button>
+                </form>
+                {/* <input onChange={e => this.handleAddContent(e)}placeholder='content'></input>
+                <button>submit</button> */}
             </>
         )
     }
